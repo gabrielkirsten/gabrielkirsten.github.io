09c21100843fdd5f941443275fd2fd4347e061b0
Gabriel Kirsten <gabriel.kirsten@hotmail.com>
2019-03-27 17:23:01 -0400

    9617ef2

---
+++ precache-manifest.d84e9a0e2e71b4abafbdf93434452eb0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "2a935b614a3a95263328",
+    "url": "/js/chunk-vendors.1ec66500.js"
+  },
+  {
+    "revision": "4a2331a89612c44edd9a",
+    "url": "/js/app.6a06b43d.js"
+  },
+  {
+    "revision": "ea4c6847510a0b7c7ce27611dd4667e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a2331a89612c44edd9a",
+    "url": "/css/app.72147282.css"
+  }
+];
